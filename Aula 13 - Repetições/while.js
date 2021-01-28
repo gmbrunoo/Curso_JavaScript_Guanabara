c5c3c92a162e7aefeafdeab01d483e9260d2571a
Bruno Godinho <gm.brunoo@gmail.com>
2021-01-28 17:25:16 -0300

    Aula 13 - Repetição

---
+++ Aula 13 - Repetições/while.js
@@ -0,0 +1,14 @@
+
+// WHILE
+var fatia = 8
+  while (fatia > 0){
+    console.log('Fatia comida')
+    fatia--;
+  }
+
+// DO WHILE 
+var fatia = 8
+  do {
+    console.log('Fatia comida')
+    fatia--;
+  }while (fatia > 0)
